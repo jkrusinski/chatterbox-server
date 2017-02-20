ff7731cdf9d145cc57ba9cc7e8c111ed78ba62b5
jkrusinski <jkruzr@gmail.com>
2017-02-20 15:24:55 -0600

    All tests passing.

--- server/request-handler.js
+++ server/request-handler.js
@@ -12,83 +12,10 @@ this file and include it in basic-server.js so that it actually works.
 **************************************************************/
 var db = require('diskdb');
 var jsonBody = require('body/json');
+
 // creates messages collection in database
 db = db.connect(__dirname + '/db', ['messages']);
 
-var requestHandler = function(request, response) {
-  // Request and Response come from node's http module.
-  //
-  // They include information about both the incoming request, such as
-  // headers and URL, and about the outgoing response, such as its status
-  // and content.
-  //
-  // Documentation for both request and response can be found in the HTTP section at
-  // http://nodejs.org/documentation/api/
-
-  // Do some basic logging.
-  //
-  // Adding more logging to your server can be an easy way to get passive
-  // debugging help, but you should always be careful about leaving stray
-  // console.logs in your code.
-  console.log('Serving request type ' + request.method + ' for url ' + request.url);
-
-  // The outgoing status.
-  var routeOkay = request.url.substring(0, 17) === '/classes/messages';
-  // See the note below about CORS headers.
-  var headers = defaultCorsHeaders;
-  headers['Content-Type'] = 'application/json';
-
-  // Tell the client we are sending them plain text.
-  //
-  // You will need to change this if you are sending something
-  // other than plain text, like JSON or HTML.
-
-  // .writeHead() writes to the request line and headers of the response,
-  // which includes the status and all headers.
-  
-  if (request.method === 'GET' && routeOkay) {
-
-    var messages = db.messages.find();
-    response.writeHead(200, headers);
-    response.end(`{"results":${JSON.stringify(messages)}}`);
-  }
-
-  if (request.method === 'POST' && routeOkay) {
-
-    jsonBody(request, response, function(err, body) {
-      if (err) {
-        throw new Error();
-      }
-
-      db.messages.save(body);
-
-      response.writeHead(201, headers);
-      response.end('{"message": "success!"}');
-    });
-  } else {
-
-    response.writeHead(404, headers);
-    response.end();
-  }
-
-  // Make sure to always call response.end() - Node may not send
-  // anything back to the client until you do. The string you pass to
-  // response.end() will be the body of the response - i.e. what shows
-  // up in the browser.
-  //
-  // Calling .end "flushes" the response's internal buffer, forcing
-  // node to actually send all the data over to the client.
-};
-
-// These headers will allow Cross-Origin Resource Sharing (CORS).
-// This code allows this server to talk to websites that
-// are on different domains, for instance, your chat client.
-//
-// Your chat client is running from a url like file://your/chat/client/index.html,
-// which is considered a different domain.
-//
-// Another way to get around this restriction is to serve you chat
-// client from this domain by setting up static file serving.
 var defaultCorsHeaders = {
   'access-control-allow-origin': '*',
   'access-control-allow-methods': 'GET, POST, PUT, DELETE, OPTIONS',
@@ -96,5 +23,47 @@ var defaultCorsHeaders = {
   'access-control-max-age': 10 // Seconds.
 };
 
-module.exports = requestHandler;
+var requestHandler = function(request, response) {
+
+  console.log('Serving request type ' + request.method + ' for url ' + request.url);
+
+  var routeOkay = request.url.substring(0, 17) === '/classes/messages';
+  var headers = defaultCorsHeaders;
+  
+  headers['Content-Type'] = 'application/json';
+
+  
+  if (request.method === 'GET' && routeOkay) {
+
+    var messages = db.messages.find();
+    response.writeHead(200, headers);
+    response.end(`{"results":${JSON.stringify(messages)}}`);
+
+  } else if (request.method === 'POST' && routeOkay) {
+
+    var body = [];
+
+    request.on('data', function(chunk) {
+
+      body.push(chunk);
+
+    });
+    request.on('end', function() {
+
+      body = Buffer.concat(body).toString();
+      db.messages.save(JSON.parse(body));
+
+      response.writeHead(201, headers);
+      response.end('{"message": "success"}');
+    });
+
+  } else {
+
+    response.writeHead(404, headers);
+    response.end();
+  }
+
+};
+
+exports.requestHandler = requestHandler;
 
