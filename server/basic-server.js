dac99d32afd789c337373924ac936e959bb09d0d
jkrusinski <jkruzr@gmail.com>
2017-02-20 19:46:22 -0600

    Implemented express

--- server/basic-server.js
+++ server/basic-server.js
@@ -1,42 +1,68 @@
-/* Import node's http module: */
-var http = require('http');
-var handleRequest = require('./request-handler');
+var express = require('express');
+var app = express();
+
+var db = require('diskdb');
+db = db.connect(__dirname + '/db', ['messages']);
+
+var bodyParser = require('body-parser');
 
 
-// Every server needs to listen on a port with a unique number. The
-// standard port for HTTP servers is port 80, but that port is
-// normally already claimed by another server and/or not accessible
-// so we'll use a standard testing port like 3000, other common development
-// ports are 8080 and 1337.
-var port = 3000;
+app.use(function (req, res, next) {
+  res.header('access-control-allow-origin', '*');
+  res.header('access-control-allow-methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('access-control-allow-headers', 'content-type, accept');
+  res.header('access-control-max-age', 10);
 
-// For now, since you're running this server on your local machine,
-// we'll have it listen on the IP address 127.0.0.1, which is a
-// special address that always refers to localhost.
-var ip = '127.0.0.1';
+  if ('OPTIONS' === req.method) {
+
+    res.send(200);
+
+  } else {
+
+    // move on
+    next();
+  }
+});
+
+app.use(bodyParser.json());
+
+app.get('/classes/messages', function (req, res) {
+
+  var messages = {};
+  messages.results = db.messages.find();
+
+  messages.results.sort((a, b) => {
+    if (a.createdAt < b.createdAt) {
+      return 1;
+    }
+    if (a.createdAt > b.createdAt) {
+      return -1;
+    }
+    return 0;
+  });
+
+  res.json(messages);
+});
+
+app.post('/classes/messages', function (req, res) {
+
+  var message = req.body;
+
+  message.createdAt = new Date();
+
+  db.messages.save(message);
+
+  res.status(201).end();
+  
+});
+
+app.use(function (req, res, next) {
+  res.status(404).send('Sorry can\'t find that!');
+});
 
 
+app.listen(3000, function () {
 
-// We use node's http module to create a server.
-//
-// The function we pass to http.createServer will be used to handle all
-// incoming requests.
-//
-// After creating the server, we will tell it to listen on the given port and IP. */
-var server = http.createServer(handleRequest.requestHandler);
-console.log('Listening on http://' + ip + ':' + port);
-server.listen(port, ip);
+  console.log('Server is listening on port 3000.');
 
-// To start this server, run:
-//
-//   node basic-server.js
-//
-// on the command line.
-//
-// To connect to the server, load http://127.0.0.1:3000 in your web
-// browser.
-//
-// server.listen() will continue running as long as there is the
-// possibility of serving more requests. To stop your server, hit
-// Ctrl-C on the command line.
-
+});
